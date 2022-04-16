3755def2b22c5d3ead3bc64fdd4d338bf67d0bd5
Mamurov97 <70318096+Mamurov97@users.noreply.github.com>
2022-04-16 10:54:07 +0500

    next commit

--- lib/features/buyurtma/data/datasources/buyurtma_remote_datasource.dart
+++ lib/features/buyurtma/data/datasources/buyurtma_remote_datasource.dart
@@ -1,32 +1,32 @@
-// import 'package:savdo_agnet_client/features/buyurtma/data/model/buyurtma_model.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
-//
-// abstract class BuyurtmaRemoteDataSource {
-//   Future<List<BuyurtmaModel>> getBuyurtma(int buyurtmaId);
-//
-// }
-//
-// class BuyurtmaRemoteDataSourceImpl implements BuyurtmaRemoteDataSource {
-//   final SharedPreferences sharedPreferences;
-//
-//
-//   BuyurtmaRemoteDataSourceImpl(
-//       {required this.sharedPreferences});
-//
-//   @override
-//   Future<List<BuyurtmaModel>> getBuyurtma(int buyurtmaId) {
-//     // TODO: implement getBuyurtma
-//     throw UnimplementedError();
+import 'package:savdo_agnet_client/features/buyurtma/data/model/buyurtma_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+abstract class BuyurtmaRemoteDataSource {
+  Future<List<BuyurtmaModel>> getBuyurtma(int buyurtmaId);
+
+}
+
+class BuyurtmaRemoteDataSourceImpl implements BuyurtmaRemoteDataSource {
+  final SharedPreferences sharedPreferences;
+
+
+  BuyurtmaRemoteDataSourceImpl(
+      {required this.sharedPreferences});
+
+  @override
+  Future<List<BuyurtmaModel>> getBuyurtma(int buyurtmaId) {
+    // TODO: implement getBuyurtma
+    throw UnimplementedError();
+  }
+
+
+// @override
+// Future<List<ClientModel>> getClient() async {
+//   List<ClientModel> list = [];
+//   try {
+//     return true;
+//   } on LocalFailure {
+//     return false;
 //   }
-//
-//
-// // @override
-// // Future<List<ClientModel>> getClient() async {
-// //   List<ClientModel> list = [];
-// //   try {
-// //     return true;
-// //   } on LocalFailure {
-// //     return false;
-// //   }
-// // }
 // }
+}
--- lib/features/buyurtma/data/repository/buyurtma_repository.dart
+++ lib/features/buyurtma/data/repository/buyurtma_repository.dart
@@ -5,32 +5,35 @@ import 'package:savdo_agnet_client/features/buyurtma/data/datasources/buyurtma_l
 import 'package:savdo_agnet_client/features/buyurtma/domain/entity/buyurtma_entity.dart';
 import 'package:savdo_agnet_client/features/buyurtma/domain/repositories/buyurtma_repository.dart';
 
+import '../datasources/buyurtma_remote_datasource.dart';
+
 class BuyurtmaRepositoryImpl extends BuyurtmaRepository {
   final BuyurtmaLocalDataSourceImpl localDataSourceImpl;
-
-  // final BuyurtmaRemoteDataSourceImpl remoteDataSourceImpl;
+  final BuyurtmaRemoteDataSourceImpl remoteDataSourceImpl;
   final NetworkInfo networkInfo;
 
   BuyurtmaRepositoryImpl({
     required this.localDataSourceImpl,
     required this.networkInfo,
-    // required this.remoteDataSourceImpl,
+    required this.remoteDataSourceImpl,
   });
 
   @override
-  Future<Either<Failure, List<BuyurtmaEntity>>> getBuyurtma(int buyurtmaId) {
-    // TODO: implement getBuyurtma
-    throw UnimplementedError();
+  Future<Either<Failure, List<BuyurtmaEntity>>> getBuyurtma(int buyurtmaId) async {
+    if (await networkInfo.isConnected) {
+      try {
+        final result = await remoteDataSourceImpl.getBuyurtma(buyurtmaId);
+        return Right(result);
+      } on ServerFailure {
+        return const Left(ServerFailure("Маълумот юкланишда хатолик бўлди"));
+      }
+    } else {
+      try {
+        final result = await localDataSourceImpl.getBuyurtma(buyurtmaId);
+        return Right(result);
+      } on LocalFailure {
+        return const Left(ServerFailure("Маълумот юкланишда хатолик бўлди"));
+      }
+    }
   }
-//
-// @override
-// Future<Either<Failure, dynamic>> getBuyurtma(int buyurtmaId) async {
-//
-//   try {
-//     final result = localDataSourceImpl.getBuyurtma(buyurtmaId);
-//     return Right(result);
-//   } on ServerFailure {
-//     return const Left(ServerFailure("Маълумот юкланишда хатолик бўлди"));
-//   }
-// }
 }
